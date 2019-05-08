51b93bf73f5f2253b923f6acabf00e826b53a958
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-05-08 21:17:23 +0300

    form validation added

--- app/script.js
+++ app/script.js
@@ -1,3 +1,4 @@
+// navigation scroll below
 $('a[href*="#"]').on('click', function(e) {
     e.preventDefault()
 
@@ -9,3 +10,20 @@ $('a[href*="#"]').on('click', function(e) {
     'linear'
     )
 })
+
+// form validaton below
+function validateFormA() {
+    var x = document.forms["freeTrialForm"]["firstname", "lastname", "phonenumber"].value;
+    if (x == "") {
+      alert("Please fill out all fields.");
+      return false;
+    }
+  }
+
+function validateFormB() {
+    var xx = document.forms["contactForm"]["firstnamecont", "lastnamecont", "phonenumbercont"].value;
+    if (xx == "") {
+      alert("Please fill out all fields.");
+      return false;
+    }
+  }
