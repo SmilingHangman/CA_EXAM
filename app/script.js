137162fd2499ec36967ca75dbbb66da41fc41018
SmilingHangman <andrejus.chomicenka@gmail.com>
2019-05-09 21:46:29 +0300

    reviews section kinda done

--- app/script.js
+++ app/script.js
@@ -21,9 +21,43 @@ function validateFormA() {
   }
 
 function validateFormB() {
-    var xx = document.forms["contactForm"]["firstnamecont", "lastnamecont", "phonenumbercont"].value;
-    if (xx == "") {
+    var x = document.forms["contactForm"]["firstnamecont", "lastnamecont", "phonenumbercont"].value;
+    if (x == "") {
       alert("Please fill out all fields.");
       return false;
     }
   }
+
+// swiper
+// var mySwiper = new Swiper ('.swiper-container', {
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// })
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  spaceBetween: 60,
+  direction: 'horizontal',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
